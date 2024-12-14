907a29efa9db5f4ec234646e81959cfdb23c9b34
aashish_k <aashishkathat00@gmail.com>
2024-12-14 06:59:38 +0530

    added settings for deploying on vercel

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,7 +1,7 @@
 import db from '@repo/database';
 
 export default async function Page() {
-   const user = await db.user.findMany()
+   const user = await db.user.findMany();
 
    return (
       <main className="flex flex-col items-center justify-between min-h-screen p-24">
